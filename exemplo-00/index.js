f83723f9c7c731fe5a8eebc803707d2a2c7473ce
Giancarlo Haack Albano <gian418@gmail.com>
2026-02-21 14:21:35 -0300

    parte 2

--- exemplo-00/index.js
+++ exemplo-00/index.js
@@ -68,15 +68,15 @@ async function trainModel(inputXs, outputYs) {
     return model
 }
 
-// async function predict(model, pessoa) {
-//     // transformar o array js para o tensor (tfjs)
-//     const tfInput = tf.tensor2d(pessoa)
+async function predict(model, pessoa) {
+    // transformar o array js para o tensor (tfjs)
+    const tfInput = tf.tensor2d(pessoa)
 
-//     // Faz a predição (output será um vetor de 3 probabilidades)
-//     const pred = model.predict(tfInput)
-//     const predArray = await pred.array()
-//     return predArray[0].map((prob, index) => ({ prob, index }))
-// }
+    // Faz a predição (output será um vetor de 3 probabilidades)
+    const pred = model.predict(tfInput)
+    const predArray = await pred.array()
+    return predArray[0].map((prob, index) => ({ prob, index }))
+}
 // Exemplo de pessoas para treino (cada pessoa com idade, cor e localização)
 // const pessoas = [
 //     { nome: "Erick", idade: 30, cor: "azul", localizacao: "São Paulo" },
@@ -118,25 +118,25 @@ const outputYs = tf.tensor2d(tensorLabels)
 // dos dados
 const model = await trainModel(inputXs, outputYs)
 
-// const pessoa = { nome: 'zé', idade: 28, cor: 'verde', localizacao: "Curitiba" }
-// // Normalizando a idade da nova pessoa usando o mesmo padrão do treino
-// // Exemplo: idade_min = 25, idade_max = 40, então (28 - 25) / (40 - 25 ) = 0.2
+const pessoa = { nome: 'zé', idade: 28, cor: 'verde', localizacao: "Curitiba" }
+// Normalizando a idade da nova pessoa usando o mesmo padrão do treino
+// Exemplo: idade_min = 25, idade_max = 40, então (28 - 25) / (40 - 25 ) = 0.2
 
-// const pessoaTensorNormalizado = [
-//     [
-//         0.2, // idade normalizada
-//         1,    // cor azul
-//         0,    // cor vermelho
-//         0,    // cor verde
-//         0,    // localização São Paulo
-//         1,    // localização Rio
-//         0     // localização Curitiba
-//     ]
-// ]
+const pessoaTensorNormalizado = [
+    [
+        0.2, // idade normalizada
+        1,    // cor azul
+        0,    // cor vermelho
+        0,    // cor verde
+        0,    // localização São Paulo
+        1,    // localização Rio
+        0     // localização Curitiba
+    ]
+]
 
-// const predictions = await predict(model, pessoaTensorNormalizado)
-// const results = predictions
-//     .sort((a, b) => b.prob - a.prob)
-//     .map(p => `${labelsNomes[p.index]} (${(p.prob * 100).toFixed(2)}%)`)
-//     .join('\n')
-// console.log(results)
+const predictions = await predict(model, pessoaTensorNormalizado)
+const results = predictions
+    .sort((a, b) => b.prob - a.prob)
+    .map(p => `${labelsNomes[p.index]} (${(p.prob * 100).toFixed(2)}%)`)
+    .join('\n')
+console.log(results)
